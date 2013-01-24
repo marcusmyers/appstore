ac3c02a7047ead42c077f00fdeb03dc9ce4cdff7
Mark Myers <marcusmyers@gmail.com>
2013-01-24 17:52:45 -0500

    First Commit

---
+++ public/error.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>ERROR</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+
+</head>
+<body>
+<br/><br/>
+<div class="container">
+<div class="row">
+        <div class="alert alert-error">
+            <h2>Error!?!</h2>
+            You have reached this page in error.
+        </div>
+</div>
+</body>
+</html>
---
+++ public/login.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>NACS App Store</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="NACS App Store">
+    <meta name="author" content="technology@napoleonareaschools.org">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+     <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+        background-color: #f5f5f5;
+	background: url('/img/dgrey023.gif');
+      }
+
+      .form-signin {
+        max-width: 450px;
+        padding: 19px 29px 29px;
+        margin: 80px auto 20px;
+        background-color: #d5d5d5;
+        border: 1px solid #e5e5e5;
+        -webkit-border-radius: 5px;
+           -moz-border-radius: 5px;
+                border-radius: 5px;
+        -webkit-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+           -moz-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+                box-shadow: 0 1px 2px rgba(0,0,0,.05);
+      }
+      .form-signin .form-signin-heading,
+      .form-signin .checkbox {
+        margin-bottom: 10px;
+      }
+      .form-signin input[type="text"],
+      .form-signin input[type="password"] {
+        font-size: 16px;
+        height: auto;
+        margin-bottom: 15px;
+        padding: 7px 9px;
+      }
+
+    </style>
+    <link href="css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="css/login.css" rel="stylesheet">
+</head>
+<body>
+     <div class="container">
+
+      <form class="form-signin">
+        <h2 class="form-signin-heading">Please sign in</h2>
+        <input type="text" class="input-block-level" placeholder="Username">
+        <input type="password" class="input-block-level" placeholder="Password">
+        <label class="checkbox">
+          <input type="checkbox" value="remember-me"> Remember me
+        </label>
+        <button class="btn btn-large btn-primary" type="submit">Sign in</button>
+      </form>
+
+    </div> <!-- /container -->
+</body>
+</html>
